8c8ca6991def620b22409013a250bab722395cdf
oejaramillo <oscar.10000@hotmail.com>
2024-01-25 15:27:01 -0300

    database definition heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://toogsvldqxpbyq:56b14ed8c0385df9ac9133b4ca490926811f90fa9a3d2aeb1e842c4af81abdd7@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d610b7rqbdimb4'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
