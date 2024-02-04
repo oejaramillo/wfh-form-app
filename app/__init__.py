d2b594738a5d66347b7aa3ce4d8c745a39a779ab
oejaramillo <oscar.10000@hotmail.com>
2024-02-04 20:24:21 -0300

    database url

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vcvcjaecvjtmzs:4b1e1cd1f29058cf3f6d7214e689a3cc06709c3d7725d64e20f2bbe712494479@ec2-54-234-13-16.compute-1.amazonaws.com:5432/d2h0bn6k19adl1'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
