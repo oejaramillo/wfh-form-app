effb9a10bf68df8179abab2e7b3355c8be8badbc
oejaramillo <oscar.10000@hotmail.com>
2024-09-29 17:29:42 -0400

    database script

--- init_db.py
+++ init_db.py
@@ -2,6 +2,7 @@ from app import db, app  # Import the db instance and app instance
 
 def init_db():
     with app.app_context():
+        db.drop_all()
         db.create_all()
 
 if __name__ == '__main__':
