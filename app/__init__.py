19b063e09cd180f15d46601492ef4ab88be0c7a4
oejaramillo <oscar.10000@hotmail.com>
2024-09-29 17:17:56 -0400

    init.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 # Get the DATABASE_URL, replace "postgres://" with "postgresql://" due to heroku
-#database_url = os.environ.get("DATABASE_URL")
-#if database_url.startswith("postgres://"):
-#   database_url = database_url.replace("postgres://", "postgresql://", 1)
+database_url = os.environ.get("DATABASE_URL")
+if database_url.startswith("postgres://"):
+   database_url = database_url.replace("postgres://", "postgresql://", 1)
     
-#app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
